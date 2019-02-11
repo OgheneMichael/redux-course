6ed21453e6e6e0247ccfdc8be85a36c07787bec3
OgheneMichael <zonalmeet@gmail.com>
2019-02-11 21:20:05 +0100

    create-react-app

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,11 @@ import React, { Component } from "react";
 
 class App extends Component {
 	render() {
-		return <div>hello world</div>;
+		return (
+			<div>
+				<h1>hello world</h1>
+			</div>
+		);
 	}
 }
 
